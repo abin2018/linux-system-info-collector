86f8277f7f82aa70663982d79cc1b6521986b486
lvwan <lvwan@lvwan.com>
2021-01-19 17:27:07 +0800

    set the parameter

--- process_hosts_info.py
+++ process_hosts_info.py
@@ -3,18 +3,26 @@
 
 import os
 import json
+import sys
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 
-def get_host_info(host_info_file):
+def get_host_info_dict(host_info_file):
     info_dict = {}
     with open(host_info_file) as f:
         info_dict = json.load(f)
     return info_dict
 
+def output_json(result_dir):
+    output_list = []
+    for file in os.listdir(result_dir):
+        host = os.path.splitext(file)[0]
+	file_path = os.path.join(result_dir, file)
+	info_dict = get_host_info_dict(file_path)
+        output_list.append(info_dict)
+    print(json.dumps(output_list, indent=4))
 
-if __name__ == '__main__':
-    result_dir = os.path.join(BASEDIR, '.result')
+def output_table(result_dir):
     out_tag_title = "|%-15s|%-15s|%-20s|%-19s|%-14s|%s|%-8s|%-52s|%-23s|%-23s|"
     out_tag = "|%-15s|%-13s|%-18s|%-20s|%s|%-3s|%-6s|%-50s|%-20s|%-20s|"
     print('-'*164)
@@ -22,7 +30,7 @@ if __name__ == '__main__':
     for file in os.listdir(result_dir):
         host = os.path.splitext(file)[0]
 	file_path = os.path.join(result_dir, file)
-	info_dict = get_host_info(file_path)
+	info_dict = get_host_info_dict(file_path)
         info_dict['host'] = host
 	print('-'*164)
 	print(out_tag % (
@@ -34,3 +42,16 @@ if __name__ == '__main__':
             ' '.join(['{}:{}*{}({} {})'.format(i["vd_name"], i["number_of_drivers"], i["raw_size"], i["pd_type"], i["raid_level"]) for i in info_dict["raid_info"]]),
 	))
     print('-'*164)
+
+
+if __name__ == '__main__':
+    try:
+        output_format = sys.argv[1]
+    except Exception:
+        print("Usage: {} output_format".format(__file__))
+        sys.exit()
+    result_dir = os.path.join(BASEDIR, '.result')
+    if output_format == 'json':
+        output_json(result_dir)
+    else:
+        output_table(result_dir)
--- run.sh
+++ run.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 #General Variables
-BASEDIR=$(cd $(dirname $0) ; pwd)
-HOSTS_FILE=$BASEDIR/hosts
-RESULT_DIR=$BASEDIR/.result
-IGNORE_HOSTS=$BASEDIR/.ignore_hosts
-export PATH=$PATH:/usr/sbin
+BASE_DIR=$(cd $(dirname $0) ; pwd)
+HOSTS_FILE=""
+RESULT_DIR=$BASE_DIR/.result
+IGNORE_HOSTS=$BASE_DIR/.ignore_hosts
+OUTPUT_FORMAT="table"
 
-[ -f ${HOSTS_FILE} ] || touch ${HOSTS_FILE}
-[ -d ${RESULT_DIR} ] && rm -rf ${RESULT_DIR}
-mkdir ${RESULT_DIR}
-
-ALL_HOSTS=$(cat ${HOSTS_FILE})
-ALL_HOSTS_ARRAY=(${ALL_HOSTS})
-ALL_HOSTS_ARRAY_INDEX=0
-HOSTS_COUNT=$(cat ${HOSTS_FILE} | wc -l)
+function prepare() {
+    if [ -z "${HOSTS_FILE}" ] ; then
+        HOSTS_FILE=$BASE_DIR/.empty
+        touch ${HOSTS_FILE}
+    fi
+    ALL_HOSTS=$(cat ${HOSTS_FILE})
+    ALL_HOSTS_ARRAY=(${ALL_HOSTS})
+    ALL_HOSTS_ARRAY_INDEX=0
+    HOSTS_COUNT=$(cat ${HOSTS_FILE} | wc -l)
+    [ -d ${RESULT_DIR} ] && rm -rf ${RESULT_DIR}
+    mkdir ${RESULT_DIR}
+}
 
 function distribute_script() {
     host=$1
-    scp -o ConnectTimeout=3 -r $BASEDIR/scripts $host:/tmp/ &>/dev/null
+    scp -o ConnectTimeout=3 -r $BASE_DIR/scripts $host:/tmp/ &>/dev/null
     [ $? -ne 0 ] && { echo "$host connect failed"; echo $host >> ${IGNORE_HOSTS}; }  #将失败的主机写入到一个文件中
 }
 
@@ -49,8 +53,61 @@ function call_run_remote_cmd() {
     wait
 }
 
-distribute_all_scripts
-echo "开始收集主机信息，请稍等 "
-call_run_remote_cmd 'bash /tmp/scripts/run.sh get_json'
-python $BASEDIR/process_hosts_info.py
-clean_all
+function usage() {
+    cat <<eof
+Usage: $0 [OPTION] [FILE]
+options
+  -f format                  set the output format, valid option is 'json' or 'table', default is 'table'
+  -h hostfile                a text file that contains all hosts
+eof
+}
+
+function args_parser() {
+    while getopts ":f:h:" opt ; do
+        case $opt in 
+            f)
+                OUTPUT_FORMAT=$OPTARG
+                if echo ${OUTPUT_FORMAT} | grep -q '^-.*' ; then
+                    usage
+                    exit
+                fi 
+                if [[ ${OUTPUT_FORMAT} != 'json' ]] && [[ ${OUTPUT_FORMAT} != 'table' ]] ; then
+                    echo "only json or table is supported"
+                    exit
+                fi
+                ;;
+            h) 
+                HOSTS_FILE=$OPTARG
+                if echo ${HOSTS_FILE} | grep -q '^-.*' ; then
+                    usage
+                    exit
+                fi 
+                if [ ! -f ${HOSTS_FILE} ] ; then
+                    echo "no such file ${HOSTS_FILE}"
+                    exit
+                fi
+                ;;
+            ?)
+                usage
+                exit
+         esac
+    done
+
+}
+
+function main() {
+    if [ -z "${ALL_HOSTS}" ] ; then
+        /bin/bash $BASE_DIR/scripts/run.sh get_json > ${RESULT_DIR}/localhost.json 2>/dev/null
+        return 1
+    else
+        distribute_all_scripts
+        echo "开始收集主机信息，请稍等 "
+        call_run_remote_cmd 'bash /tmp/scripts/run.sh get_json' 2>/dev/null
+        clean_all
+    fi
+}
+
+args_parser "$@"
+prepare
+main
+python $BASE_DIR/process_hosts_info.py ${OUTPUT_FORMAT}
