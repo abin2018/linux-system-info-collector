8caf51ea7d3e73e3ba54094b61b18fbd150c0470
lvwan <lvwan@lvwan.com>
2021-01-19 21:03:08 +0800

    repair the output

--- process_hosts_info.py
+++ process_hosts_info.py
@@ -5,7 +5,7 @@ import os
 import json
 import sys
 
-BASEDIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def get_host_info_dict(host_info_file):
     info_dict = {}
@@ -17,31 +17,43 @@ def output_json(result_dir):
     output_list = []
     for file in os.listdir(result_dir):
         host = os.path.splitext(file)[0]
-	file_path = os.path.join(result_dir, file)
-	info_dict = get_host_info_dict(file_path)
+        file_path = os.path.join(result_dir, file)
+        info_dict = get_host_info_dict(file_path)
         output_list.append(info_dict)
     print(json.dumps(output_list, indent=4))
 
+def full_field(rows, ilist):
+    for i in range((rows - len(ilist))):
+        ilist.append(' '*22)
+
 def output_table(result_dir):
-    out_tag_title = "|%-15s|%-15s|%-20s|%-19s|%-14s|%s|%-8s|%-52s|%-23s|%-23s|"
-    out_tag = "|%-15s|%-13s|%-18s|%-20s|%s|%-3s|%-6s|%-50s|%-20s|%-20s|"
-    print('-'*164)
+    out_tag_title = "|%-15s|%-15s|%-27s|%-22s|%-44s|%s|%-8s|%-24s|%-24s|%-22s|"
+    out_tag = "|%-15s|%-13s|%-25s|%-20s|%-40s|%-3s|%-6s|%-22s|%-22s|%-22s|"
+    print('-'*199)
     print(out_tag_title % ("IP", "厂商", "型号","类型", "操作系统", "CPU", "内存", "网卡", "硬盘", "RAID"))
+    print('-'*199)
     for file in os.listdir(result_dir):
         host = os.path.splitext(file)[0]
-	file_path = os.path.join(result_dir, file)
-	info_dict = get_host_info_dict(file_path)
+        file_path = os.path.join(result_dir, file)
+        info_dict = get_host_info_dict(file_path)
         info_dict['host'] = host
-	print('-'*164)
-	print(out_tag % (
-            info_dict["host"], info_dict["product_info"]["sys_vendor"], info_dict["product_info"]["product_name"],
-            info_dict["server_type"], info_dict["os_info"], info_dict["cpu_info"]["cpu_processor_count"], 
-            info_dict["memory_info"], ' '.join(['{}({})'.format(i["interface_name"], i["interface_speed"]) for i in info_dict["net_interface_info"]]),
-            #' '.join(['{}:{}({})'.format(i["disk_name"], i["disk_size"], i["disk_type"]) for i in info_dict["disk_info"]]),
-            "sda:6.0TB(SATA) sdb:6.0TB(SATA)\n"+" "*440+"sdc:6.0TB(SATA) sdd:6.0TB(SATA)\n"+" "*440+"sde:6.0TB(SATA) sdf:6.0TB(SATA)\n"+" "*440+"sdg:600GB(SATA)"+" "*16,
-            ' '.join(['{}:{}*{}({} {})'.format(i["vd_name"], i["number_of_drivers"], i["raw_size"], i["pd_type"], i["raid_level"]) for i in info_dict["raid_info"]]),
-	))
-    print('-'*164)
+        line_fix = [info_dict["host"], info_dict["product_info"]["sys_vendor"], info_dict["product_info"]["product_name"],
+                             info_dict["server_type"], info_dict["os_info"], info_dict["cpu_info"]["cpu_processor_count"],info_dict["memory_info"]]
+        net_interface_list = ['{}({})'.format(i["interface_name"], i["interface_speed"]) for i in info_dict["net_interface_info"]]
+        disk_list = ['{}:{}({})'.format(i["disk_name"], i["disk_size"], i["disk_type"]) for i in info_dict["disk_info"]]
+        raid_list = ['{}:{}*{}({} {})'.format(i["vd_name"], i["number_of_drivers"], i["raw_size"], i["pd_type"], i["raid_level"]) for i in info_dict["raid_info"]]
+        rows = max(len(net_interface_list), len(disk_list), len(raid_list))
+        full_field(rows, net_interface_list)
+        full_field(rows, disk_list)
+        full_field(rows, raid_list)
+        line_fix.append(net_interface_list[0])
+        line_fix.append(disk_list[0])
+        line_fix.append(raid_list[0])
+        print(out_tag % tuple(line_fix))
+        for i in range(1, rows):
+            output_line = [" "*15, " "*13, " "*18, " "*20, " "*40, " "*3, " "*6, net_interface_list[i], disk_list[i], raid_list[i]]
+            print(out_tag % tuple(output_line))
+        print('-'*199)
 
 
 if __name__ == '__main__':
@@ -50,7 +62,7 @@ if __name__ == '__main__':
     except Exception:
         print("Usage: {} output_format".format(__file__))
         sys.exit()
-    result_dir = os.path.join(BASEDIR, '.result')
+    result_dir = os.path.join(BASE_DIR, '.result')
     if output_format == 'json':
         output_json(result_dir)
     else:
